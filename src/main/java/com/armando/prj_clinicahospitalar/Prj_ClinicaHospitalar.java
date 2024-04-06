eb9f063c3be85e79c1306e0b2b963c415420684e
Armando Tavares <armandomoraistavares@gmail.com>
2024-04-06 15:22:35 -0300

    first commit

---
+++ src/main/java/com/armando/prj_clinicahospitalar/Prj_ClinicaHospitalar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.armando.prj_clinicahospitalar;
+
+/**
+ *
+ * @author Armando
+ */
+public class Prj_ClinicaHospitalar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
